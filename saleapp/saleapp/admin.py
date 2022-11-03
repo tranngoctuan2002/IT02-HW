a1afded322afa9b4657d46e8095e36980be12eae
Ngoc Tuan <tranngoctuan20102002@gmail.com>
2022-11-04 00:05:25 +0700

    add authenticated

--- saleapp/saleapp/admin.py
+++ saleapp/saleapp/admin.py
@@ -1,26 +1,44 @@
-from saleapp.models import Category, Product
+from saleapp.models import Category, Product, UserRole
 from saleapp import app, db
+from flask import redirect
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
+from flask_login import logout_user, current_user
 
 admin = Admin(app=app, name='Quản trị cơ sở dữ liệu', template_mode='Bootstrap4')
 
-class ProductView(ModelView):
+class AuthenticatedModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.user_role == UserRole.ADMIN
+
+class AuthenticatedView(BaseView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+class ProductView(AuthenticatedModelView):
     column_searchable_list = ['name']
     column_filters = ['name', 'price']
     column_exclude_list = ['image']
-    column_details_list = True
+    can_view_details = True
+    can_export = True
+    column_export_list = ['name', 'description', 'price']
     column_labels = {
         'name': 'Tên sản phẩm',
         'description': 'Mô tả',
         'price': 'Giá'
     }
 
-class StatsView(BaseView):
+class StatsView(AuthenticatedView):
     @expose('/')
     def index(self):
         return self.render('admin/stats.html')
 
-admin.add_view(ModelView(Category, db.session, name='Danh mục'))
+class LogoutView(AuthenticatedView):
+    @expose('/')
+    def index(self):
+        logout_user()
+        return redirect('/admin')
+
+admin.add_view(AuthenticatedModelView(Category, db.session, name='Danh mục'))
 admin.add_view(ProductView(Product, db.session, name='Sản phẩm'))
 admin.add_view(StatsView(name='Thống kê - báo cáo'))
+admin.add_view(LogoutView(name='Đăng xuất'))
